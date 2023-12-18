340ceb641cea4383f03000f269e4dd7d7780a8a4
EvanWilliams5352 <143012664+EvanWilliams5352@users.noreply.github.com>
2023-12-18 10:02:07 -0700

    website

---
+++ SkiingJava.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var myBox = document.getElementById("myBox");
+    var myLink = document.getElementById("myLink");
+
+    myBox.addEventListener("click", function () {
+        alert("Box Clicked!");
+        // You can replace the alert with any other action you want.
+    });
+});
